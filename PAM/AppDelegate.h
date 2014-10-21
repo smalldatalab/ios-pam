7e926a98f243bcac375a703e96979dc408cb2281
Charlie Forkish <cforkish@gmail.com>
2014-10-21 14:38:45 -0700

    Initial Commit

---
+++ PAM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PAM
+//
+//  Created by Charles Forkish on 10/21/14.
+//  Copyright (c) 2014 Charlie Forkish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PAM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PAM
+//
+//  Created by Charles Forkish on 10/21/14.
+//  Copyright (c) 2014 Charlie Forkish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
